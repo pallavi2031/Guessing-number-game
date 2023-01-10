c4873e06cfb1cd8d54d15a6fa05f4b77fbcb3705
pallavi <guptapallavi460@gmail.com>
2023-01-10 20:57:28 +0530

    guessing-number-game

---
+++ Guessing_number.js
@@ -0,0 +1,15 @@
+let a=45;
+let num;
+
+do{
+  num=prompt("enter the num between 1 to 100");
+    count++;
+if(num>a){
+    console.log("your num is greater try again!");
+    }
+  else if(num<a){
+       console.log("your num is smaller try again!");
+  }
+  }while(num!=a);
+  console.log("!!!--------conguratulation------!!!","you     guess correct num");
+  
